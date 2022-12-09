a0f9505560d120ddf7f87014d774cbc41147bf85
InakiPeet <inaki.peeters@edu.vlerick.com>
2022-12-09 13:32:24 +0000

    change_1

--- assignment/assignment.py
+++ assignment/assignment.py
@@ -1,8 +1,10 @@
+import pandas as pd
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
 
 BUCKET = "dmacademy-course-assets"
-KEY = "airflow/data.csv"
+KEYpre = "vlerick/pre_release.csv"
+KEYafter = "vlerick/after_release.csv"
 
 config = {
     "spark.jars.packages": "org.apache.hadoop:hadoop-aws:3.3.1",
@@ -11,6 +13,10 @@ config = {
 conf = SparkConf().setAll(config.items())
 spark = SparkSession.builder.config(conf=conf).getOrCreate()
 
-df = spark.read.csv(f"s3a://{BUCKET}/{KEY}", header=True)
-df.show()
+dfpre = spark.read.csv(f"s3a://{BUCKET}/{KEYpre}", header=True)
+dfafter = spark.read.csv(f"s3a://{BUCKET}/{KEYafter}", header=True)
+dfpre.show()
+dfafter.show()
 
+df_pre = dfpre.toPandas()
+df_after = dfafter.toPandas()
