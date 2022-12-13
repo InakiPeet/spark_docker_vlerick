762eed948ba620e4d9f057ae88174f869b8b9e8e
InakiPeet <inaki.peeters@edu.vlerick.com>
2022-12-13 19:13:26 +0000

    dockertje_6

--- assignment/assignment.py
+++ assignment/assignment.py
@@ -1,15 +1,22 @@
 import pandas as pd
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
+import os
 
 BUCKET = "dmacademy-course-assets"
 KEYpre = "vlerick/pre_release.csv"
 KEYafter = "vlerick/after_release.csv"
 
-config = {
-    "spark.jars.packages": "org.apache.hadoop:hadoop-aws:3.3.1",
-    "spark.hadoop.fs.s3a.aws.credentials.provider": "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider",
-}
+if len(os.environ.get('AWS_SECRET_ACCESS_KEY')) < 1:
+    config = {
+        "spark.jars.packages": "org.apache.hadoop:hadoop-aws:3.3.1",
+        "spark.hadoop.fs.s3a.aws.credentials.provider": "org.apache.hadoop.fs.s3a.InstanceProfileCredentialsProvider"
+        }
+else:
+    config = {
+        "spark.jars.packages": "org.apache.hadoop:hadoop-aws:3.3.1"
+    }
+
 conf = SparkConf().setAll(config.items())
 spark = SparkSession.builder.config(conf=conf).getOrCreate()
 
